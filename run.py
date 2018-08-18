baf6c719766a0fd6f448d7630dc5e97689dba18b
TongyanXu <tyxu18@gmail.com>
2018-08-18 16:47:16 +0800

    added 2 more stickers

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ By modifying configs, wechat components may perform differently
 To see detailed info of each component, please check components repository
 """
 __author__ = 'Tongyan Xu'
-__version__ = '1.0.12'
+__version__ = '1.0.13'
 
 from wxpy import Bot, embed
 from constants import WechatComponentType, WechatDefaultConfig
