c4cffdee7cfeec347fc334c8b9b440fedc2ef5b7
TongyanXu <tyxu18@gmail.com>
2018-08-20 20:20:33 +0800

    added status monitor on wechat

---
+++ ex_tools/status_monitor.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+"""Status monitor on wechat, to report progress / status of project on wechat"""
+
+from wxpy import get_wechat_logger
+from logging import INFO
+from datetime import datetime
+
+wechat_logger = get_wechat_logger(name='status_monitor', level=INFO)
+time_format = '%Y-%m-%d %H:%M:%S'
+
+
+def status_monitor_on_wechat(func_):
+    """Decorator to send wechat messages to file helper when the decorated function is called and finished"""
+    def inner_func(*args, **kwargs):
+        """Decorated function with general input"""
+        _name = str(func_).split(' ')[1]
+        _begin = datetime.now()
+        wechat_logger.info('Start running function <{}> at {}'.format(_name, datetime.strftime(_begin, time_format)))
+
+        try:
+            _res = func_(*args, **kwargs)
+            _end = datetime.now()
+            _last = _end - _begin
+            wechat_logger.info('Finish running function <{}> at {}, lasting {} seconds\nResult: {}'.format(
+                _name, datetime.strftime(_end, time_format), _last.total_seconds(), str(_res)))
+            return _res
+
+        except Exception as e:
+            _msg = str(e)
+            _end = datetime.now()
+            _last = _end - _begin
+            wechat_logger.error('Failed to run <{}> at {}, lasting {} seconds\nError msg: {}'.format(
+                _name, datetime.strftime(_end, time_format), _last.total_seconds(), _msg))
+            raise Exception(_msg)
+
+    return inner_func
---
+++ ex_tools/test_scripts/wechat_monitor_test.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+"""Test - wechat monitor"""
+
+from ex_tools.status_monitor import status_monitor_on_wechat
+from logging import ERROR
+
+
+@status_monitor_on_wechat
+def test_function_1(input_num):
+    """Test function 1"""
+    return input_num + 1
+
+
+@status_monitor_on_wechat
+def test_function_2(input_num):
+    """Test function 2"""
+    return input_num - 1
+
+
+@status_monitor_on_wechat
+def test_function_3(input_num):
+    """Test function 3"""
+    _res = test_function_1(input_num)
+    return test_function_2(_res)
+
+
+@status_monitor_on_wechat
+def test_function_4(input_num):
+    """Test function 4"""
+    return input_num / 0
+
+
+@status_monitor_on_wechat
+def test_function_5(input_num):
+    """Test function 5"""
+    return test_function_4(input_num)
+
+
+if __name__ == '__main__':
+    test_num = 0
+    test_function_1(test_num)
+    test_function_2(test_num)
+    test_function_3(test_num)
+    try:
+        test_function_4(test_num)
+    except Exception as e:
+        print(e)
+    test_function_5(test_num)
